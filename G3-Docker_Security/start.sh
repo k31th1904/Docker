9ecc8f4e149faf6182d56f7b7c811e6a306beadb
EC2 Default User <ec2-user@ip-172-31-81-240.ec2.internal>
2023-05-25 00:35:41 -0400

    Updated G3

--- G3-Docker_Security/start.sh
+++ G3-Docker_Security/start.sh
@@ -1,3 +1,14 @@
+echo "[*]List contents"
+echo ""
+ls -l
+echo ""
+echo "[*]Show app.py"
+cat app.py
+echo ""
+echo "[*]Show Dockerfile"
+echo ""
+cat Dockerfile
+echo ""
 echo "[*]Show app.py is working" 
 echo "[*]Executing command : docker run --rm --name g3 --read-only g3:latest"
 echo ""
@@ -12,11 +23,12 @@ echo ""
 echo ""
 echo "[*]Show that it is read-only filesystem"
 echo "[*]Executing command : docker run --rm -it --name g3 --read-only g3:latest chmod a+x app.py"
+echo "[*]Try to modify permission ... "
 echo ""
 docker run --rm -it --name g3 --read-only g3:latest chmod a+x app.py
 echo ""
 echo ""
 echo "[*]Show that pandas is installed"
-echo "[*]Executing command : docker run --rm -it --name g3 --read-only g3:latest pip list"
+echo "[*]Executing command : docker run --rm -it --name g3 --read-only g3:latest pip list | grep pandas"
 echo ""
-docker run --rm -it --name g3 --read-only g3:latest pip list
+docker run --rm -it --name g3 --read-only g3:latest pip list | grep pandas
