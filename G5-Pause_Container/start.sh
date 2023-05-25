b0c08c2bee283b6833df912762ad11495bc13421
EC2 Default User <ec2-user@ip-172-31-81-240.ec2.internal>
2023-05-25 00:59:53 -0400

    updated G5

--- G5-Pause_Container/start.sh
+++ G5-Pause_Container/start.sh
@@ -1,8 +1,18 @@
+docker stop -t 1 pause 2> /dev/null
+docker stop -t 1 container1 2> /dev/null
+docker stop -t 1 container2 2> /dev/null
+echo ""
+cat Dockerfile
+echo ""
+docker images
+echo ""
 docker network rm devops
 docker network create devops
+echo ""
 docker network ls
 echo ""
 docker run -d --rm --name pause --net devops pause
 docker run -itd --rm --name container1 --net devops busybox
 docker run -itd --rm --name container2 --net devops busybox
+echo ""
 docker ps
